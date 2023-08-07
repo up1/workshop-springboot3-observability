5a3cab0639845a0e7cf31697688ff3adce130612
up1 <somkiat.p@gmail.com>
2023-08-07 14:39:46 +0700

    Fix bug

--- src/main/java/com/example/observability/HelloController.java
+++ src/main/java/com/example/observability/HelloController.java
@@ -3,17 +3,31 @@ package com.example.observability;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientException;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class HelloController {
 
-    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+    private final Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @Autowired
+    private RestTemplateBuilder restTemplateBuilder;
 
     @GetMapping("/hi")
     public String sayHi() {
         logger.info("Called sayHi");
+
+        try {
+            String result = restTemplateBuilder.build().getForObject("http://localhost:8081/xyz", String.class);
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+
         return "Hello from spring boot 3";
     }
 
--- src/main/java/com/example/observability/ObservabilityApplication.java
+++ src/main/java/com/example/observability/ObservabilityApplication.java
@@ -2,12 +2,20 @@ package com.example.observability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class ObservabilityApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ObservabilityApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate xxx() {
+		return new RestTemplateBuilder().build();
 	}
 
 }

1ed1eb91dec12d920f086c83cc932d0f898f602a
up1 <somkiat.p@gmail.com>
2023-08-07 13:38:06 +0700

    Initial project

---
+++ src/main/java/com/example/observability/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.observability;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    private Logger logger = LoggerFactory.getLogger(HelloController.class);
+
+    @GetMapping("/hi")
+    public String sayHi() {
+        logger.info("Called sayHi");
+        return "Hello from spring boot 3";
+    }
+
+}

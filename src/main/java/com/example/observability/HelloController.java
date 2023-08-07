40efbec4fce43e576032a21301874d554ccbf79e
up1 <somkiat.p@gmail.com>
2023-08-07 15:14:24 +0700

    Fix tracing

--- src/main/java/com/example/observability/HelloController.java
+++ src/main/java/com/example/observability/HelloController.java
@@ -1,6 +1,7 @@
 package com.example.observability;
 
 
+import io.micrometer.observation.annotation.Observed;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,7 +22,7 @@ public class HelloController {
     @GetMapping("/hi")
     public String sayHi() {
         logger.info("Called sayHi");
-
+        step1();
         try {
             String result = restTemplateBuilder.build().getForObject("http://localhost:8081/xyz", String.class);
         }catch (Exception e) {
@@ -29,6 +30,17 @@ public class HelloController {
         }
 
         return "Hello from spring boot 3";
+    }
+
+    @Observed(name = "user.name",
+            contextualName = "getting-user-name",
+            lowCardinalityKeyValues = {"userType", "userType2"})
+    private void step1() {
+        try {
+            Thread.sleep(1000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }

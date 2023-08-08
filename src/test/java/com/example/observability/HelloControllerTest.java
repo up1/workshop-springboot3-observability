f8de1228715722bcd7b3114f44c0255639986582
up1 <somkiat.p@gmail.com>
2023-08-08 15:30:52 +0700

    Add more test

--- src/test/java/com/example/observability/HelloControllerTest.java
+++ src/test/java/com/example/observability/HelloControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.observability;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -7,7 +8,8 @@ import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyInt;
+import static org.mockito.Mockito.*;
 import static org.springframework.boot.test.context.SpringBootTest.*;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
@@ -20,10 +22,20 @@ class HelloControllerTest {
     private DemoService demoService;
 
     @Test
+    @DisplayName("ทดสอบนะ")
     void sayHi2() {
         when(demoService.getData()).thenReturn("Hello5");
         HelloResponse result =
                 restTemplate.getForObject("/hi2", HelloResponse.class);
         assertEquals("Hello5", result.getMessage());
+    }
+
+    @Test
+    @DisplayName("ทดสอบนะ 2")
+    void sayHi22() {
+        when(demoService.getData()).thenReturn("Hello5");
+        HelloResponse result =
+                restTemplate.getForObject("/hi2", HelloResponse.class);
+        verify(demoService, times(1)).getData();
     }
 }

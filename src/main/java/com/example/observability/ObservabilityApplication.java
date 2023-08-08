dbcc0be7326f347319ae7f66d83f9b04610fcbd6
up1 <somkiat.p@gmail.com>
2023-08-08 11:49:49 +0700

    Working with virtual thread java 20+

--- src/main/java/com/example/observability/ObservabilityApplication.java
+++ src/main/java/com/example/observability/ObservabilityApplication.java
@@ -2,9 +2,15 @@ package com.example.observability;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.task.TaskExecutionAutoConfiguration;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.boot.web.embedded.tomcat.TomcatProtocolHandlerCustomizer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.core.task.support.TaskExecutorAdapter;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.concurrent.Executors;
 
 @SpringBootApplication
 public class ObservabilityApplication {
@@ -16,6 +22,18 @@ public class ObservabilityApplication {
 	@Bean
 	public RestTemplate xxx() {
 		return new RestTemplateBuilder().build();
+	}
+
+	@Bean(TaskExecutionAutoConfiguration.APPLICATION_TASK_EXECUTOR_BEAN_NAME)
+	public AsyncTaskExecutor asyncTaskExecutor() {
+		return new TaskExecutorAdapter(Executors.newVirtualThreadPerTaskExecutor());
+	}
+
+	@Bean
+	public TomcatProtocolHandlerCustomizer<?> protocolHandlerVirtualThreadExecutorCustomizer() {
+		return protocolHandler -> {
+			protocolHandler.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
+		};
 	}
 
 }
